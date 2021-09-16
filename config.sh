8cbd053bb55381bcff41ebdb6eb266b2f4aadcfe
PhiDCH <phi9b2@gmail.com>
2021-09-16 21:29:06 +0700

    asdasd

--- config.sh
+++ config.sh
@@ -4,8 +4,8 @@ cd ..
 pip install torch==1.7.1+cu101 torchvision==0.8.2+cu101 torchaudio==0.7.2 -f https://download.pytorch.org/whl/torch_stable.html
 pip install mmcv-full -f https://download.openmmlab.com/mmcv/dist/cu101/torch1.7.0/index.html
 pip install mmdet
-git clone https://github.com/PhiDCH/mmocr
-cd mmocr
+git clone https://github.com/PhiDCH/mmocr mmocr1
+cd mmocr1
 pip install -r requirements.txt
 pip install -v -e .  # or "python setup.py develop"
 export PYTHONPATH=$(pwd):$PYTHONPATH
@@ -28,9 +28,9 @@ unzip data.zip -d textClassify/
 rm data.zip
 
 
-git clone https://github.com/PhiDCH/CRAFT-pytorch
+git clone https://github.com/PhiDCH/CRAFT-pytorch CRAFTpytorch
 cd CRAFT-pytorch 
-gdown --id 1RyaO4cxaV62L78AEAvrrPeaX9_JLLqXB -O craft_mlt_25k.pth
+gdown --id 1V7X_ZS4ZhuZhCxbhhKO8B_1PGWQyKfdI -O craft_mlt_25k.pth
 cd ..
 
 git clone https://github.com/PhiDCH/text-recognition
